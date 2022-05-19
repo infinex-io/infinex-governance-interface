114e364d1edff86eada4d335fab07391e3f2fa8a
fritzschoff <max.j.f@gmx.de>
2022-05-19 10:01:41 +0200

    added council card

--- components/CouncilCard/index.tsx
+++ components/CouncilCard/index.tsx
@@ -1,5 +1,14 @@
+import { Button } from '@synthetixio/ui';
+import { H2 } from 'components/Headlines/H2';
 import { H4 } from 'components/Headlines/H4';
+import NominateModal from 'components/Modals/Nominate';
+import { Text } from 'components/Text/text';
+import { TransparentText } from 'components/Text/transparent';
+import Modal from 'containers/Modal';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
 
 interface CouncilCardProps {
 	cta: string;
@@ -19,81 +28,93 @@ interface CouncilCardProps {
 	headlineLeft: string;
 	headlineRight: string;
 	secondButton?: string | undefined;
+	period: 'ADMINISTRATION' | 'VOTING' | 'NOMINATION' | 'EVALUATION';
+	nomineesCount?: number;
+	membersCount?: number;
 }
 
-export default function CouncilCard({cta, button, variant, color, headlineLeft, headlineRight, secondButton}: CouncilCardProps) {
+export default function CouncilCard({
+	cta,
+	button,
+	variant,
+	color,
+	headlineLeft,
+	headlineRight,
+	secondButton,
+	period,
+	nomineesCount,
+	membersCount,
+}: CouncilCardProps) {
+	const { t } = useTranslation();
+	const { push } = useRouter();
+	const { setContent, setIsOpen } = Modal.useContainer();
 	return (
 		<div className="bg-purple p-1">
 			<div className="h-full flex flex-col justify-around align-center darker-60">
 				<Image alt="spartan-council" src="/logos/spartan-council.svg" width={50} height={72} />
 				<H4>{t('landing-page.cards.sc')}</H4>
-						<StyledCTALabel color={spartanCouncilInfo.color}>
-							{t(spartanCouncilInfo.cta)}
-						</StyledCTALabel>
-						<StyledSpacer />
-						<div className="flex justify-around">
-							<Text>{t(spartanCouncilInfo.headlineLeft)}</Text>
-							<Text>{t(spartanCouncilInfo.headlineRight)}</Text>
-						</div>
-						<div className="flex justify-around">
-							<H2>
-								{spartanCurrentPeriod.currentPeriod === 'NOMINATION' ||
-								spartanCurrentPeriod.currentPeriod === 'VOTING'
-									? spartanNominees.data?.length
-									: spartanMembers.data?.length}
-							</H2>
-							{/* TODO @DEV implement votes received or live votes when available */}
-							<H2>
-								{spartanCurrentPeriod.currentPeriod === 'NOMINATION'
-									? spartanNominees.data?.length
-									: spartanMembers.data?.length}
-							</H2>
-						</div>
-							<TransparentText
-								gradient="lightBlue"
-								onClick={() => {
-									push({
-										pathname: '/councils',
-										query: {
-											council: 'spartan',
-											nominees: true,
-										},
-									});
-								}}
-								clickable
-							>
-								{t(spartanCouncilInfo.secondButton)}
-							</TransparentText>
-						<Button
-							variant={spartanCouncilInfo.variant}
-							className="w-full"
-							size="lg"
-							// variant={spartanCouncilInfo.variant}
-							onClick={() => {
-								if (spartanCurrentPeriod.currentPeriod === 'NOMINATION') {
-									setContent(<NominateModal />);
-									setIsOpen(true);
-								} else if (spartanCurrentPeriod.currentPeriod === 'VOTING') {
-									push({
-										pathname: '/vote',
-										query: {
-											council: 'spartan',
-										},
-									});
-								} else {
-									push({
-										pathname: '/councils',
-										query: {
-											council: 'spartan',
-										},
-									});
-								}
-							}}
-						>
-							{t(spartanCouncilInfo.button)}
-						</Button>
-					</>
+				<span className={`bg-${color} p-1`}>{t(cta)}</span>
+				<StyledSpacer />
+				<div className="flex justify-around">
+					<Text>{t(headlineLeft)}</Text>
+					<Text>{t(headlineRight)}</Text>
+				</div>
+				<div className="flex justify-around">
+					<H2>{period === 'NOMINATION' || period === 'VOTING' ? nomineesCount : membersCount}</H2>
+					{/* TODO @DEV implement votes received or live votes when available */}
+					<H2>{period === 'NOMINATION' ? nomineesCount : membersCount}</H2>
+				</div>
+				{secondButton && (
+					<TransparentText
+						gradient="lightBlue"
+						onClick={() => {
+							push({
+								pathname: '/councils',
+								query: {
+									council: 'spartan',
+									nominees: true,
+								},
+							});
+						}}
+						clickable
+					>
+						{t(secondButton)}
+					</TransparentText>
+				)}
+				<Button
+					variant={variant}
+					className="w-full"
+					size="lg"
+					onClick={() => {
+						if (period === 'NOMINATION') {
+							setContent(<NominateModal />);
+							setIsOpen(true);
+						} else if (period === 'VOTING') {
+							push({
+								pathname: '/vote',
+								query: {
+									council: 'spartan',
+								},
+							});
+						} else {
+							push({
+								pathname: '/councils',
+								query: {
+									council: 'spartan',
+								},
+							});
+						}
+					}}
+				>
+					{t(button)}
+				</Button>
 			</div>
 		</div>
 	);
 }
+
+const StyledSpacer = styled.span`
+	height: 1px;
+	width: 203px;
+	background: linear-gradient(73.6deg, #8e2de2 2.11%, #4b01e0 90.45%);
+`;
