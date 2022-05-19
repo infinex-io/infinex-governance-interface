2d218a36fd97c05c0d1e13d95beb8eb0758d0a12
AndyTCF <andy.c.f.98@gmail.com>
2022-05-19 14:44:01 +1000

    extend nomination period dates query

--- queries/epochs/useNominationPeriodDatesQuery.ts
+++ queries/epochs/useNominationPeriodDatesQuery.ts
@@ -1,21 +1,41 @@
 import { useQuery } from 'react-query';
 import { useModulesContext } from 'containers/Modules';
 import { DeployedModules } from 'containers/Modules';
+import { ethers } from 'ethers';
 
 type NominationPeriodDates = {
 	nominationPeriodStartDate: number;
+	nominationPeriodEndDate: number;
 };
 
-function useNominationPeriodDatesQuery(moduleInstance: DeployedModules) {
+function useNominationPeriodDatesQuery(moduleInstance: DeployedModules, epochIndex?: string) {
 	const governanceModules = useModulesContext();
 
 	return useQuery<NominationPeriodDates>(
 		['nominationPeriodDates', moduleInstance],
 		async () => {
 			const contract = governanceModules[moduleInstance]?.contract;
-			const nominationPeriodStartDate =
-				Number(await contract?.getNominationPeriodStartDate()) * 1000;
-			return { nominationPeriodStartDate };
+			let nominationPeriodStartDate;
+			let nominationPeriodEndDate;
+			if (epochIndex) {
+				nominationPeriodStartDate =
+					Number(
+						await contract?.getNominationPeriodStartDateForIndex(
+							ethers.BigNumber.from(epochIndex).toHexString()
+						)
+					) * 1000;
+				nominationPeriodEndDate =
+					Number(
+						await contract?.getVotingPeriodStartDateForIndex(
+							ethers.BigNumber.from(epochIndex).toHexString()
+						)
+					) * 1000;
+			} else {
+				nominationPeriodStartDate = Number(await contract?.getNominationPeriodStartDate()) * 1000;
+				nominationPeriodEndDate = Number(await contract?.getVotingPeriodStartDate()) * 1000;
+			}
+
+			return { nominationPeriodStartDate, nominationPeriodEndDate };
 		},
 		{
 			enabled: governanceModules !== null && moduleInstance !== null,
