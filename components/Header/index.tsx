d66aa847ae8b6f5a82a063e2e2c8327f5f0e8041
fritzschoff <fritzschoff@icloud.com>
2022-05-31 09:54:56 +0200

    remvoed burger menu

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -8,12 +8,12 @@ import useCurrentPeriod from 'queries/epochs/useCurrentPeriodQuery';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
-import { slide as BurgerMenu } from 'react-burger-menu';
-import './header.module.scss';
+import { truncateAddress } from 'utils/truncate-address';
 
 export default function Header() {
 	const { push, pathname } = useRouter();
 	const { t } = useTranslation();
+	const [burgerMenuOpen, setBurgerMenuOpen] = useState(false);
 	const [activeRoute, setActiveRoute] = useState('home');
 	const [routes, setRoutes] = useState([
 		t('header.routes.home'),
@@ -47,22 +47,29 @@ export default function Header() {
 	};
 
 	useEffect(() => {
+		if (burgerMenuOpen) {
+			document.documentElement.classList.add('stop-scrolling');
+		} else document.documentElement.classList.remove('stop-scrolling');
+	}, [burgerMenuOpen]);
+
+	useEffect(() => {
 		const splitRoute = pathname.split('/')[1];
 		setActiveRoute(splitRoute ? splitRoute : 'home');
 	}, [pathname]);
 
 	return (
 		<>
-			<header className="bg-dark-blue w-full m-h-[66px] p-3 flex items-center justify-center border-b-gray-800 border-b-[1px] border-b-solid">
+			<header className="bg-dark-blue w-full m-h-[66px] p-3 flex items-center md:justify-center justify-between border-b-gray-800 border-b-[1px] border-b-solid">
 				<Link href="/" passHref>
 					<div className="flex items-center cursor-pointer mr-8">
 						<SNXIcon />
 						<StyledHeaderHeadline>Governance</StyledHeaderHeadline>
 					</div>
 				</Link>
-				<div className="invisible md:visible flex justify-center w-full">
+				<div className="hidden md:flex justify-center w-full">
 					{routes.map((translation, index) => (
-						<StyledSpotlightButton
+						<SpotlightButton
+							className="last-of-type:m-auto"
 							text={translation}
 							onClick={() => handleIndexAndRouteChange(index)}
 							active={activeRoute === translation.toLowerCase()}
@@ -70,6 +77,45 @@ export default function Header() {
 						/>
 					))}
 				</div>
+				<button
+					className="md:hidden flex flex-col items-center max-width-[30px] max-h-[30px]"
+					onClick={() => setBurgerMenuOpen(!burgerMenuOpen)}
+				>
+					{!burgerMenuOpen ? (
+						<>
+							<span className="min-w-[30px] h-[2px] bg-white m-1 rounded"></span>
+							<span className="min-w-[30px] h-[2px] bg-white m-1 rounded"></span>
+							<span className="min-w-[30px] h-[2px] bg-white m-1 rounded"></span>
+						</>
+					) : (
+						<svg
+							width="36"
+							height="36"
+							viewBox="0 0 36 36"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						></svg>
+					)}
+				</button>
+				{burgerMenuOpen && (
+					<div className="fixed w-full h-full z-100 bg-black bg-opacity-70 top-[66px] left-0">
+						<div className="flex flex-col items-center">
+							{routes.map((translation, index) => (
+								<SpotlightButton
+									className="m-4"
+									size="lg"
+									text={translation}
+									onClick={() => {
+										handleIndexAndRouteChange(index);
+										setBurgerMenuOpen(!burgerMenuOpen);
+									}}
+									active={activeRoute === translation.toLowerCase()}
+									key={translation}
+								/>
+							))}
+						</div>
+					</div>
+				)}
 				<div className="flex mr-1">
 					<Button
 						className="min-w-[143px]"
@@ -78,29 +124,13 @@ export default function Header() {
 					>
 						{ensAvatar && <StyledENSAvatar src={ensAvatar} />}
 						<StyledWalletAddress>
-							{!walletAddress
-								? t('header.connect-wallet')
-								: ensName
-								? ensName
-								: walletAddress!
-										.slice(0, 5)
-										.concat('...')
-										.concat(walletAddress.slice(walletAddress.length - 3))}
+							{ensName || !!walletAddress
+								? truncateAddress(walletAddress!)
+								: t('header.connect-wallet')}
 						</StyledWalletAddress>
 					</Button>
 				</div>
 			</header>
-			<div className="bm-burger-button"></div>
-			<BurgerMenu isOpen={false} className="md:invisible">
-				{routes.map((translation, index) => (
-					<StyledSpotlightButton
-						text={translation}
-						onClick={() => handleIndexAndRouteChange(index)}
-						active={activeRoute === translation.toLowerCase()}
-						key={translation}
-					/>
-				))}
-			</BurgerMenu>
 		</>
 	);
 }
@@ -112,13 +142,6 @@ const StyledHeaderHeadline = styled.h1`
 	font-size: 0.87rem;
 	color: white;
 	margin-left: ${theme.spacings.tiny};
-`;
-
-const StyledSpotlightButton = styled(SpotlightButton)`
-	margin-right: ${theme.spacings.medium};
-	:last-of-type {
-		margin-right: auto;
-	}
 `;
 
 const StyledWalletAddress = styled.span`
