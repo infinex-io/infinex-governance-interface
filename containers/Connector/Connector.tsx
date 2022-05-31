d84862cc138dbff2279772a89793d2d12ffccaf8
fritzschoff <fritzschoff@icloud.com>
2022-05-31 17:39:24 +0200

    refactored boardroom sign in

--- containers/Connector/Connector.tsx
+++ containers/Connector/Connector.tsx
@@ -1,73 +1,14 @@
 import React, { useEffect, useMemo, useState, useContext, createContext } from 'react';
 import { ethers } from 'ethers';
-import useLocalStorage from 'hooks/useLocalStorage';
-import {
-	BOARDROOM_SIGNIN_API_URL,
-	BOARDROOM_SIGNOUT_API_URL,
-	NONCE_API_URL,
-} from 'constants/boardroom';
-import { SiweMessage } from 'siwe';
-
 import '@rainbow-me/rainbowkit/styles.css';
-import {
-	getDefaultWallets,
-	RainbowKitProvider,
-	connectorsForWallets,
-	wallet,
-} from '@rainbow-me/rainbowkit';
-import {
-	chain,
-	configureChains,
-	createClient,
-	useAccount,
-	useProvider,
-	useSigner,
-	WagmiConfig,
-} from 'wagmi';
+import { RainbowKitProvider, connectorsForWallets, wallet } from '@rainbow-me/rainbowkit';
+import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
 import { infuraProvider } from 'wagmi/providers/infura';
 import { publicProvider } from 'wagmi/providers/public';
-
-type SIWEMessage = {
-	message: {
-		domain: string;
-		address: string;
-		chainId: number;
-		uri: string;
-		version: string;
-		statement: string;
-		nonce: string;
-		issuedAt: string;
-		signature: string;
-	};
-};
-
-type SignOutResponse = {
-	data: {
-		success: boolean;
-	};
-};
-
-type NonceResponse = {
-	data: {
-		nonce: string;
-	};
-};
-
-type SignInResponse = {
-	data: {
-		ens: string;
-		address: string;
-		uuid: string;
-	};
-};
 
 type ConnectorContextType = {
 	ensName: string | null;
 	ensAvatar: string | null;
-	uuid: string | null;
-	setUuid: (value: string | null) => void;
-	boardroomSignIn: () => Promise<string | undefined>;
-	boardroomSignOut: () => void;
 	L1DefaultProvider: ethers.providers.InfuraProvider;
 	L2DefaultProvider: ({
 		chainId,
@@ -137,92 +78,15 @@ export const ConnectorContextProvider: React.FC = ({ children }) => {
 		}
 	}, [wagmiClient.data?.account, L1DefaultProvider]);
 
-	// const boardroomSignIn = async () => {
-	// 	// @TODO: change to real domain on prod
-	// 	const domain = 'localhost:3000';
-
-	// 	const chainId = 31337;
-
-	// 	if (signer && provider && account) {
-	// 		try {
-	// 			const body = {
-	// 				address: account,
-	// 			};
-	// 			let response = await fetch(NONCE_API_URL, {
-	// 				method: 'POST',
-	// 				body: JSON.stringify(body),
-	// 			});
-	// 			const nonceResponse: NonceResponse = await response.json();
-
-	// 			let signedMessage = new SiweMessage({
-	// 				domain: domain,
-	// 				address: account,
-	// 				chainId: chainId,
-	// 				uri: `http://${domain}`,
-	// 				version: '1',
-	// 				statement: 'Sign into Boardroom with this wallet',
-	// 				nonce: nonceResponse.data.nonce,
-	// 				issuedAt: new Date().toISOString(),
-	// 			});
-
-	// 			const signature = await provider.getSigner().signMessage(signedMessage.prepareMessage());
-
-	// 			const message = {
-	// 				message: { ...signedMessage, signature },
-	// 			} as SIWEMessage;
-
-	// 			response = await fetch(BOARDROOM_SIGNIN_API_URL, {
-	// 				method: 'POST',
-	// 				body: JSON.stringify(message),
-	// 			});
-
-	// 			const signInResponse: SignInResponse = await response.json();
-
-	// 			setUuid(signInResponse.data.uuid);
-
-	// 			return signInResponse.data.uuid;
-	// 		} catch (e) {
-	// 			console.log(e);
-	// 		}
-	// 	}
-	// };
-
-	// const boardroomSignOut = async () => {
-	// 	if (account && uuid) {
-	// 		try {
-	// 			const body = {
-	// 				address: account,
-	// 				uuid: uuid,
-	// 			};
-	// 			let response = await fetch(BOARDROOM_SIGNOUT_API_URL, {
-	// 				method: 'POST',
-	// 				body: JSON.stringify(body),
-	// 			});
-
-	// 			const { data }: SignOutResponse = await response.json();
-
-	// 			if (data.success) {
-	// 				setUuid(null);
-	// 			}
-	// 		} catch (e) {
-	// 			console.log(e);
-	// 		}
-	// 	}
-	// };
-
 	return (
 		<WagmiConfig client={wagmiClient}>
 			<RainbowKitProvider chains={chains}>
 				<ConnectorContext.Provider
 					value={{
-						// uuid,
-						// setUuid,
 						ensAvatar,
 						ensName,
 						provider,
 						chains,
-						// boardroomSignIn,
-						// boardroomSignOut,
 						L1DefaultProvider,
 						L2DefaultProvider: provider,
 					}}
--- mutations/boardroom/useUpdateUserDetailsMutation.ts
+++ mutations/boardroom/useUpdateUserDetailsMutation.ts
@@ -1,18 +1,99 @@
-import { UPDATE_USER_DETAILS_API_URL } from 'constants/boardroom';
-import { useConnectorContext } from 'containers/Connector';
+import {
+	BOARDROOM_SIGNIN_API_URL,
+	NONCE_API_URL,
+	UPDATE_USER_DETAILS_API_URL,
+} from 'constants/boardroom';
 import useIsUUIDValidQuery from 'queries/boardroom/useIsUUIDValidQuery';
 import { GetUserDetails } from 'queries/boardroom/useUserDetailsQuery';
 import { useMutation, useQueryClient } from 'react-query';
-import { useAccount } from 'wagmi';
+import { SiweMessage } from 'siwe';
+import { useAccount, useProvider, useSigner } from 'wagmi';
 
 type UpdateUserDetailsResponse = {
 	data: GetUserDetails & {
 		uuid: string;
 	};
 };
+type SIWEMessage = {
+	message: {
+		domain: string;
+		address: string;
+		chainId: number;
+		uri: string;
+		version: string;
+		statement: string;
+		nonce: string;
+		issuedAt: string;
+		signature: string;
+	};
+};
+
+type NonceResponse = {
+	data: {
+		nonce: string;
+	};
+};
+
+type SignInResponse = {
+	data: {
+		ens: string;
+		address: string;
+		uuid: string;
+	};
+};
 
 function useUpdateUserDetailsMutation() {
-	const { uuid, boardroomSignIn } = useConnectorContext();
+	const { data: signer } = useSigner();
+	const provider = useProvider();
+	const account = useAccount();
+	const boardroomSignIn = async () => {
+		// TODO @andy: change to real domain on prod
+		const domain = 'localhost:3000';
+
+		const chainId = 31337;
+
+		if (signer && provider && account.data?.address) {
+			try {
+				const body = {
+					address: account.data?.address,
+				};
+				let response = await fetch(NONCE_API_URL, {
+					method: 'POST',
+					body: JSON.stringify(body),
+				});
+				const nonceResponse: NonceResponse = await response.json();
+
+				let signedMessage = new SiweMessage({
+					domain: domain,
+					address: account.data.address,
+					chainId: chainId,
+					uri: `http:${domain}`,
+					version: '1',
+					statement: 'Sign into Boardroom with this wallet',
+					nonce: nonceResponse.data.nonce,
+					issuedAt: new Date().toISOString(),
+				});
+
+				const signature = await signer.signMessage(signedMessage.prepareMessage());
+
+				const message = {
+					message: { ...signedMessage, signature },
+				} as SIWEMessage;
+
+				response = await fetch(BOARDROOM_SIGNIN_API_URL, {
+					method: 'POST',
+					body: JSON.stringify(message),
+				});
+
+				const signInResponse: SignInResponse = await response.json();
+
+				return signInResponse.data.uuid;
+			} catch (e) {
+				console.log(e);
+			}
+		}
+	};
+
 	const { data } = useAccount();
 	const isUuidValidQuery = useIsUUIDValidQuery();
 	const queryClient = useQueryClient();
@@ -27,7 +108,7 @@ function useUpdateUserDetailsMutation() {
 			if (address) {
 				const body = {
 					...userProfile,
-					uuid: signedInUuid || uuid,
+					uuid: signedInUuid,
 				};
 				let response = await fetch(UPDATE_USER_DETAILS_API_URL(address), {
 					method: 'POST',
