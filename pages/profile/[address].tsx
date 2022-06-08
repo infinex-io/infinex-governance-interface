8fbe849d73629ad0b22685ac166c86a52373ed0e
AndyTCF <andy.c.f.98@gmail.com>
2022-06-08 16:46:26 +1000

    better handling when wallet address does not exists

--- pages/profile/[address].tsx
+++ pages/profile/[address].tsx
@@ -1,13 +1,15 @@
 import Main from 'components/Main';
-import Error from 'next/error';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import ProfileSection from 'components/Profile';
 import { isWalletAddress } from 'utils/validate';
+import { Text } from 'components/Text/text';
+import { t } from 'i18next';
+import { Loader } from 'components/Loader/Loader';
 
 export default function Profile() {
-	const { query } = useRouter();
-	const { address } = query;
+	const router = useRouter();
+	const { address } = router.query;
 
 	return (
 		<>
@@ -15,8 +17,23 @@ export default function Profile() {
 				<title>Synthetix | {address ? address : 'Profile'}</title>
 			</Head>
 			<Main>
-				{typeof address === 'string' && isWalletAddress(address) && (
-					<ProfileSection walletAddress={address} />
+				{router.isReady ? (
+					typeof address === 'string' && isWalletAddress(address) ? (
+						<ProfileSection walletAddress={address} />
+					) : (
+						<div className="flex flex-col align-center gap-4">
+							<h1 className="tg-title-h1 c text-center">
+								{t('profiles.profile-not-found.header')}
+							</h1>
+							<Text>
+								{t(`profiles.profile-not-found.subheadline`, {
+									walletAddress: address,
+								})}
+							</Text>
+						</div>
+					)
+				) : (
+					<Loader fullScreen />
 				)}
 			</Main>
 		</>
