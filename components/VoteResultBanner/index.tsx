d4adb2fc3016beada9e5ae9589ffe6169b305b22
Peiman <peymannaderi3@yahoo.com>
2022-06-29 14:44:24 +0430

    fix: align vote result banner on mobile

--- components/VoteResultBanner/index.tsx
+++ components/VoteResultBanner/index.tsx
@@ -20,10 +20,10 @@ export const VoteResultBanner: React.FC = () => {
 		<div className="w-full p-0.5 bg-purple rounded mx-auto">
 			<div className="flex md:flex-row flex-col gap-2 items-center justify-between w-full h-full darker-60 p-5">
 				<div className="flex md:flex-row flex-col gap-2 items-center">
-					<div className="bg-dark-blue rounded-full h-12 w-12 flex items-center justify-center mr-4">
+					<div className="bg-dark-blue rounded-full h-12 w-12 flex items-center justify-center md:mr-4">
 						<SNXIcon />
 					</div>
-					<div>
+					<div className="text-center md:text-left">
 						<h3 className="tg-title-h3">{t('landing-page.vote-banner.title')}</h3>
 						<p className="tg-content mt-1">{t('landing-page.vote-banner.subtitle')}</p>
 					</div>
