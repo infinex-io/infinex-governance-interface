56aa4810f898a82bbceb08052c3a980b4fecaa96
fritzschoff <fritzschoff@icloud.com>
2022-07-28 14:38:50 +0200

    fix(tab-icon): fixed alignment of digit

--- components/TabIcon/index.tsx
+++ components/TabIcon/index.tsx
@@ -2,14 +2,14 @@ import clsx from 'clsx';
 import { PropsWithChildren } from 'react';
 
 export const TabIcon = ({ isActive, children }: PropsWithChildren<{ isActive: boolean }>) => (
-	<span
-		className={clsx('tg-caption-sm rounded-full p-[4px] px-[8px]', {
+	<div
+		className={clsx('rounded-full w-5 h-5 flex items-center justify-center pt-0.5', {
 			'bg-black': isActive,
 			'bg-primary': !isActive,
 			'text-white': isActive,
 			'text-black': !isActive,
 		})}
 	>
-		{children}
-	</span>
+		<span className="tg-caption-sm">{children}</span>
+	</div>
 );
