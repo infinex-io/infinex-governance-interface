90dcfba8a2209ce8e3404e6d85c6f6eee4cc6bdf
fritzschoff <fritzschoff@icloud.com>
2022-07-29 09:37:10 +0200

    chore(tab-icon): combined classes

--- components/TabIcon/index.tsx
+++ components/TabIcon/index.tsx
@@ -4,10 +4,8 @@ import { PropsWithChildren } from 'react';
 export const TabIcon = ({ isActive, children }: PropsWithChildren<{ isActive: boolean }>) => (
 	<div
 		className={clsx('rounded-full w-5 h-5 flex items-center justify-center pt-0.5', {
-			'bg-black': isActive,
-			'bg-primary': !isActive,
-			'text-white': isActive,
-			'text-black': !isActive,
+			'bg-black text-white': isActive,
+			'bg-primary text-black': !isActive,
 		})}
 	>
 		<span className="tg-caption-sm">{children}</span>
