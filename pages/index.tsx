273e7a5e8b6cd79147d1e74d110ef736a4b44bb1
max <fritzschoff@icloud.com>
2023-04-12 09:00:43 +0100

    updated fork and buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
+import { useColorMode } from '@chakra-ui/react';
 import LandingPage from 'components/LandingPage';
 import Main from 'components/Main';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { useEffect } from 'react';
 
 const Home: NextPage = () => {
+	const { colorMode, toggleColorMode } = useColorMode();
+
+	useEffect(() => {
+		if (colorMode === 'light') {
+			toggleColorMode();
+		}
+	}, [colorMode, toggleColorMode]);
+
 	return (
 		<>
 			<Head>
