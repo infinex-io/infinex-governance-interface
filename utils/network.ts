d3db4c15e2b4c1444f7e9966ef89020b63bc4cec
max <fritzschoff@icloud.com>
2023-04-19 11:54:52 +0100

    fix network

--- utils/network.ts
+++ utils/network.ts
@@ -6,6 +6,6 @@ export type Network = {
 	useOvm: boolean;
 };
 export function isSupportedNetworkId(id: number | string): id is NetworkId {
-	return id == 10 || id == 1;
+	return id == 10;
 }
 export const getIsOVM = (networkId: number): boolean => !!~[10, 69].indexOf(networkId);
