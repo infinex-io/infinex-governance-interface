816eb6d6a2b0401c4e2484a541a3ba8c4e8bfffc
Contreux <52302850+Contreux@users.noreply.github.com>
2023-10-05 12:05:13 +1100

    ✨ Clean up

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -212,7 +212,7 @@ export default function Header() {
 			</Modal>
 			<div className="flex md:mr-1 h-[40px] justify-end ml-[16px]">
 				{!isWalletConnected &&
-					<div className="flex justify-center">
+					<div className="flex items-center">
 					 <ConnectButton />
 					</div>
 
