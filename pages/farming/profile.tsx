620575b2dd2314ee2b97674d98bfbe035cfbb9c2
Spaceforce <71380821+spaceforce-dev@users.noreply.github.com>
2023-10-11 17:45:08 +1100

    profile page work.

--- pages/farming/profile.tsx
+++ pages/farming/profile.tsx
@@ -8,10 +8,30 @@ import { Loader } from 'components/Loader/Loader';
 import rooms from 'utils/config/rooms';
 import { Button } from 'components/button';
 import ProfileIcon from 'components/Icons/ProfileIcon';
+import useUserFarmingQuery from 'queries/farming/useUserFarmingQuery';
+import { useEffect, useState } from 'react';
+import { formatNumberWithLocale, formatPercent } from '../../utils/numbers';
 
 export default function Profile() {
 	// const router = useRouter();
 	const address = "0x123-todo"
+
+	const exchangeIdsLowercased = rooms.map(room => room.exchange_id.toLowerCase());
+
+	const [userTotals, setUserTotals] = useState<any>({})
+	const [platformTotals, setPlatformTotals] = useState<any>({})
+
+	const userFarmingQuery = useUserFarmingQuery();
+	useEffect(() => {
+		console.log(userFarmingQuery.data)
+
+		const userTotals = userFarmingQuery?.data?.points[`points`]
+		const platformTotals = userFarmingQuery?.data?.points[`pointsPlatform`]
+
+		setUserTotals(userTotals);
+		setPlatformTotals(platformTotals);
+	}, [userFarmingQuery])
+
 
 	// todo move to component
 	return (
@@ -20,46 +40,58 @@ export default function Profile() {
 				<title>Infinex | {address ? address : 'Profile'}</title>
 			</Head>
 			<Main>
-						<div style={{height: '100%'}} className="px-8 sm:px-0 flex flex-col justify-center items-center bg-primary gap-6 text-black">
-      
-						{/* Icon */}
-						{/* <span style={{color:"black"}}><ProfileIcon></ProfileIcon></span> */}
-						
-						{/* Your Stats heading */}
-						<h1 className="text-black text-4xl font-black text-center m-2">Your Stats</h1>
+				<div style={{height: '100%'}} className="px-8 sm:px-0 flex flex-col justify-center items-center bg-primary gap-6 text-black">
+	
+					{/* Icon */}
+					{/* <span style={{color:"black"}}><ProfileIcon></ProfileIcon></span> */}
+					
+					{/* Your Stats heading */}
+					<h1 className="text-black text-4xl font-black text-center m-2">Your Stats</h1>
 
-						<p className="text-black text-base font-bold">Governance power</p>
+					<p className="text-black text-base font-bold">Governance power</p>
 
-						{/* Table */}
-						<div className="hidden sm:table border-b border-white">
-							<div className="table-row">
-							{['Governance power', 'Governance power (inflation)', '% of governance power', 'Users farming', 'Pool ends'].map((heading, index) => (
-								<div key={index} className="table-cell px-4 py-2">{heading}</div>
-							))}
-							</div>
-							<div className="table-row">
-							{['145', '145', '0.45%', '123', '12 hours'].map((data, index) => (
-								<div key={index} className="table-cell px-4 py-2">{data}</div>
-							))}
-							</div>
+					{/* Table */}
+					<div className="hidden sm:table border-b border-white">
+						<div className="table-row">
+						{[
+							'Governance power', 
+							'Governance power (inflation)', 
+							'% of governance power', 
+							'Users farming', 
+							'Pool ends'
+						].map((heading, index) => (
+							<div key={index} className="table-cell px-4 py-2">{heading}</div>
+						))}
 						</div>
-
-						{/* Collapsible table for mobile */}
-						<div className="sm:hidden flex flex-col">
-							{['Heading1', 'Heading2', 'Heading3', 'Heading4', 'Heading5'].map((heading, index) => (
-							<div key={index} className="px-4 py-2 border-b border-white">
-								{heading}: {'Data' + (index + 1)}
-							</div>
-							))}
+						<div className="table-row">
+						{[
+							formatNumberWithLocale(userTotals?.total_points), 
+							formatNumberWithLocale(userTotals?.total_points_raw),
+							formatPercent(platformTotals?.total_points / userTotals?.total_points), 
+							platformTotals?.total_user_count, 
+							'TODO ADD TIME (12 hours)'
+						].map((data, index) => (
+							<div key={index} className="table-cell px-4 py-2">{data}</div>
+						))}
 						</div>
+					</div>
 
-						{/* Your Pools heading */}
-						<h1 className="text-black text-4xl font-black text-center">Your Pools</h1>
+					{/* Collapsible table for mobile */}
+					<div className="sm:hidden flex flex-col">
+						{['Heading1', 'Heading2', 'Heading3', 'Heading4', 'Heading5'].map((heading, index) => (
+						<div key={index} className="px-4 py-2 border-b border-white">
+							{heading}: {'Data' + (index + 1)}
+						</div>
+						))}
+					</div>
 
-						{/* Pool Boxes */}
-						<div className="flex flex-wrap justify-center bg-primary">
-							{(Object.values(rooms).slice(0,4)).map((room, index) => (
-							<div key={index} style={{border: 'black solid 1px'}} className="w-full sm:w-1/2 p-4">
+					{/* Your Pools heading */}
+					<h1 className="text-black text-4xl font-black text-center">Your Pools</h1>
+
+					{/* Pool Boxes */}
+					<div className="flex flex-wrap justify-center bg-primary">
+						{ userTotals ? (Object.values(rooms)).map((room, index) => (
+							userTotals[`${room.exchange_id.toLowerCase()}`] > 0 ? <div key={index} style={{border: 'black solid 1px'}} className="w-full sm:w-1/2 p-4">
 								<div className="flex flex-col items-center p-4 bg-gray-700 rounded"  style={{background: 'rgba(0,0,0,0)'}}>
 								
 								{/* Box Heading */}
@@ -75,28 +107,26 @@ export default function Profile() {
 								<div className="grid grid-cols-2 gap-4 mt-4">
 									<div>
 										<h3 className="text-sm">{`Your points`}</h3>
-										<p>{'123'}</p>
+										<p>{formatNumberWithLocale(userTotals[`${room.exchange_id.toLowerCase()}`])}</p>
 									</div>
 									<div>
 										<h3 className="text-sm">{`Your points (inflation)`}</h3>
-										<p>{'123'}</p>
+										<p>{formatNumberWithLocale(userTotals[`${room.exchange_id.toLowerCase()}_raw`])}</p>
 									</div>
 									<div>
 										<h3 className="text-sm">{`Points in pool`}</h3>
-										<p>{'123'}</p>
+										<p>{formatNumberWithLocale(platformTotals[`${room.exchange_id.toLowerCase()}`])}</p>
 									</div>
 									<div>
 										<h3 className="text-sm">{`Users in pool`}</h3>
-										<p>{'123'}</p>
+										<p>{formatNumberWithLocale(platformTotals[`${room.exchange_id.toLowerCase()}_user_count`])}</p>
 									</div>
 								</div>
 								</div>
-							</div>
-							))}
-						</div>
-						</div>
-					)
-				)
+							</div> : ""
+						)) : "Loading..."}
+					</div>
+				</div>
 			</Main>
 		</>
 	);
