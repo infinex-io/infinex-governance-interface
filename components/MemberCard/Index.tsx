43dabc3212c299333bc73a90bf2bf0dbfd6221bc
James <jamest.1402@gmail.com>
2023-10-16 23:38:03 +1100

    fix member card bug

--- components/MemberCard/Index.tsx
+++ components/MemberCard/Index.tsx
@@ -53,92 +53,11 @@ export default function MemberCard({
 		);
 
 	if (!userDetailsQuery.data) return null;
-	console.log(userDetailsQuery.data)
-
 	const member = userDetailsQuery.data;
-
-	const socialMedia =
-		member.discord || member.twitter || member.github ? (
-			<div className="flex items-center justify-center my-3 gap-4">
-				<UserSocials discord={member.discord} twitter={member.twitter} github={member.github} />
-			</div>
-		) : (
-			<div className="my-3" />
-		);
-
-	const content = !listView ? (
-		<>
-			<Avatar width={56} height={56} walletAddress={member.address} url={member.pfpThumbnailUrl} />
-			{council && (
-				<span
-					className={`bg-[#15262A] text-[#31C690] p-2 rounded font-medium text-[11px] text-center my-2 uppercase w-fit self-center`}
-					data-testid="cta-text"
-				>
-					{t('profiles.council', { council: parseCouncil(council as "trade" | "ecosystem" | "core-contributor" | "coreContributor" | "treasury") })}
-				</span>
-			)}
-			<h5 className="tg-title-h5 capitalize">
-				{member.username || member.ens || truncateAddress(member.address)}
-			</h5>
-			<span className="opacity-75 tg-caption text-gray-200 w-full truncate text-center">
-				{member.about}
-			</span>
-			{socialMedia}
-			<div className="flex justify-around w-full relative">
-				<MemberCardAction
-					state={state}
-					isOwnCard={isOwnCard}
-					member={member}
-					votedFor={votedFor}
-					walletAddress={walletAddress}
-					deployedModule={deployedModule}
-					council={council}
-				/>
-			</div>
-		</>
-	) : (
-		<>
-			<Avatar width={40} height={40} walletAddress={member.address} url={member.pfpThumbnailUrl} />
-			<div className="relative flex flex-col flex-1 ml-3">
-				<div
-					className={clsx('flex items-center', {
-						'mb-4': member.about,
-					})}
-				>
-					<h5 className="tg-title-h5 capitalize">
-						{member.username || member.ens || truncateAddress(member.address)}
-					</h5>
-					{council && (
-						<Badge variant="blue" className="uppercase ml-4">
-							{t('profiles.council', { council })}
-						</Badge>
-					)}
-				</div>
-
-				{member.about && (
-					<p className="w-full bottom-0 left-0 pr-4 absolute opacity-75 tg-caption text-gray-200 truncate text-left">
-						{member.about}
-					</p>
-				)}
-			</div>
-			<div className="flex items-center">
-				{socialMedia}
-				<div className="flex justify-around relative ml-4">
-					<MemberCardAction
-						state={state}
-						isOwnCard={isOwnCard}
-						member={member}
-						votedFor={votedFor}
-						walletAddress={walletAddress}
-						deployedModule={deployedModule}
-						council={council}
-					/>
-				</div>
-			</div>
-		</>
-	);
+	console.log(member)
 
 	return (
+		member !== undefined && member.address !== undefined &&
 		<div
 			key={member.address.concat(member.about)}
 			className={clsx('p-0.5 rounded-lg', className, {
@@ -155,8 +74,44 @@ export default function MemberCard({
 						'flex-col': !listView,
 					}
 				)}
-			>
-				{member.address && content}
+			>	
+				{/* CONTENT WRAPPER */}
+				<>
+					<Avatar width={56} height={56} walletAddress={member.address} url={member.pfpThumbnailUrl} />
+					{council && (
+						<span
+							className={`bg-[#15262A] text-[#31C690] p-2 rounded font-medium text-[11px] text-center my-2 uppercase w-fit self-center`}
+							data-testid="cta-text"
+						>
+							{t('profiles.council', { council: parseCouncil(council as "trade" | "ecosystem" | "core-contributor" | "coreContributor" | "treasury") })}
+						</span>
+					)}
+					<h5 className="tg-title-h5 capitalize">
+						{member.username || member.ens || truncateAddress(member.address)}
+					</h5>
+					<span className="opacity-75 tg-caption text-gray-200 w-full truncate text-center">
+						{member.about}
+					</span>
+					{/* SOCIALS */}
+					{member.discord || member.twitter || member.github ? (
+						<div className="flex items-center justify-center my-3 gap-4">
+							<UserSocials discord={member.discord} twitter={member.twitter} github={member.github} />
+						</div>
+					) : (
+						<div className="my-3" />
+					)}
+					<div className="flex justify-around w-full relative">
+						<MemberCardAction
+							state={state}
+							isOwnCard={isOwnCard}
+							member={member}
+							votedFor={votedFor}
+							walletAddress={walletAddress}
+							deployedModule={deployedModule}
+							council={council}
+						/>
+					</div>
+				</>
 			</div>
 		</div>
 	);
